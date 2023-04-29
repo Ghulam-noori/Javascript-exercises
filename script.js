0ac0b39fa5974bf7d06c34ba1ca87c14a72e51c4
Ghulam-noori <131472684+Ghulam-noori@users.noreply.github.com>
2023-04-29 12:42:43 +0200

    first-commit

---
+++ script.js
@@ -0,0 +1,80 @@
+// var input = document.getElementById("item-input");
+// var button = document.getElementById("save-button");
+// var err = document.getElementById("err");
+
+// var myList = [];
+
+// button.addEventListener("click", function() {
+//   var inputValue = input.value;
+//   console.log(inputValue);
+// });
+
+// //   if (item === "") {
+// //     err.innerHTML = "<h4>Please enter an item</h4>";
+// //   }
+// //     else {
+// //     err.innerHTML = "";
+// //     myList.push();
+// //     }
+// //   });
+
+// //   function updateList() {
+// //     for (var i = 0; i < myList.length; i++) {
+// //       var newLi = document.createElement("li");
+// //       newLi.setAttribute('class', "item");
+// //       newLi.setAttribute("id", "index-" + i);
+// //       newLi.innerText = myList[i];
+
+// //       resultList.appendChild(newLi);
+
+// //     }}
+// let a = 3;
+// let b = 4;
+// console.log(a + b);
+
+
+// console.log(`half of 100 is ${100 / 2}`)
+// ;
+
+// let theNumber = Number(prompt("Pick a number"));
+// console.log("Your number is the square root of " +
+// theNumber * theNumber);
+
+// let theNumber = Number(prompt("Pick a number"));
+// if (!Number.isNaN(theNumber)) {
+// console.log("Your number is the square root of " +
+//     theNumber * theNumber);
+// }
+// else {
+//     console.log("Hey. Why didn't you give me a number?");
+//   }
+
+
+
+// let i = 1;
+// do{
+//     console.log("hi");
+//     ++i;
+// }
+// while(i <=5);
+
+// for(let i=1; i<=5; i++){
+//     console.log("hi");
+//     for(let j = 1; j<=5; j++)
+//         console.log("hello");
+// }
+// for(let i=1; i<=100; i++){
+//     if(i % 5 === 0)
+// //     console.log(i);
+// }
+// let Ash = "#";
+// while(Ash<=7){
+//     console.log(Ash);
+//     Ash++;
+// }
+// for (let line = "#"; line.length <8; line += "#")
+//   console.log(line);
+
+  for (let line = '#######'; line.length!=0; line = line.slice(0,line.length -1))
+console.log(line); 
+
